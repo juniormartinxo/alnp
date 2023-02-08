019a9dc8b7f925593836cf476b550f04fedd74d3
Junior Martins <amjr.box@gmail.com>
2023-02-07 23:00:45 -0300

    test: cria o test que vai testar o caso de uso de inserção de novo usuário
    Criado o teste que ficará responsável por testar o usecase que adiciona um novo usuário na lista de
    e-mails.

---
+++ src/usecases/register-user-on-mailing-list/register-user-on-mailing-list.spec.ts
@@ -0,0 +1,15 @@
+import { UserDataInterface } from './user-data.interface'
+
+describe('Register user on mailing list use case', () => {
+  it('should add user with complete data to mailing list', async () => {
+    const users: UserDataInterface[] = []
+    // const repo: UserRepository = new InMemoryUsersRepository(users)
+    // const useCase: RegisterUserOnMailingList = new RegisterUserOnMailingList(repo)
+    // const name = 'John Doe'
+    // const email = 'john_doe@app.com'
+    // const response = await useCase.registerUserOnMailList({ name, email })
+    // const user = repo.findUserByEmail(email)
+
+    // expect((await user).name).toBe(name)
+  })
+})
